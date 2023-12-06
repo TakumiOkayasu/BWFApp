84cc8faf375b0772f7d7afd0417730bdda424e9b
OkayasuTakumi <okayasu@daniel-soft.com>
2023-12-06 17:03:30 +0900

    ListViewからRecyclerViewに変更

---
+++ app/src/main/java/com/example/bwfapp/ui/ranking/adapter/RankingAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.bwfapp.ui.ranking.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RankingAdapter :
+    RecyclerView.Adapter<RankingAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/main/java/com/example/bwfapp/ui/ranking/adapter/RankingItem.kt
@@ -0,0 +1,5 @@
+package com.example.bwfapp.ui.ranking.adapter
+
+import android.graphics.Bitmap
+
+data class RankingItem(var rank: Int, var icon: Bitmap, var name: String, var association: String)
